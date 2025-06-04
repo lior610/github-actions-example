c110b0d33cf3e5ebfb7ffb1144e277a71386a6f8
lior <lior610.aharon@gmail.com>
2025-06-05 00:15:39 +0300

    new version

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from the sample app!');
+  res.send('This is the new app!');
 });
 
 module.exports = app;  // <-- export for tests
